01c886561fd04c598850d31c9c104497f1065185
XxN3H0M4RxX <nehomarzapata708@gmail.com>
2026-09-02 16:34:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
