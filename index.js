64d6c52b8a29692356d9aac1b8b7da54e479521d
XxN3H0M4RxX <nehomarzapata708@gmail.com>
2026-09-11 22:09:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,14 +14,13 @@ function createBot() {
     host: 'pepe2026.play.hosting',
     username: 'Bot_Antilimbo',
     version: '1.20.1',
-    // Aquí forzamos el protocolo forge y simulamos la lista de mods del servidor
     protocol: 'forge',
     forgeSendsHandshake: true,
     checkTimeout: 60000
   });
 
   bot.on('spawn', () => {
-    console.log('Bot conectado al server con mods sin pechar.');
+    console.log('Bot conectado al servidor con mods.');
     setInterval(() => {
       bot.setControlState('jump', true);
       setTimeout(() => bot.setControlState('jump', false), 500);
